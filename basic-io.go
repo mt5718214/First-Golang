3161cc2297081687d87cbfe75345a708bd2e7f12
Lin <mt5718214@gmail.com>
2021-03-01 10:55:57 +0800

    basic input/output

---
+++ basic-io.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func main(){
+	//基本輸出: fmt.Println(data, data, ...)
+	fmt.Println(3)
+
+	//基本輸出: fmt.Scanln(&data, &data, ...)
+	//&data: 取得變數的指標(Pointer)
+	var x int
+	fmt.Println("輸入一個數字")
+	fmt.Scanln(&x)
+	fmt.Println(x)
+}
