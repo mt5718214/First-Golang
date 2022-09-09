05a7cedee3325f495973b0ad91fd50313bfd9986
tsungilin <mt5718214@gmail.com>
2022-09-09 10:56:06 +0800

    add go-sql-driver module

--- gin-demo/main.go
+++ gin-demo/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type IndexData struct {
 	Title   string
 	Content string
 }
+
+type User struct {
+	id                 int
+	username, password string
+}
+
+const (
+	USERNAME = "demo"
+	PASSWORD = "demo123"
+	NETWORK  = "tcp"
+	SERVER   = "127.0.0.1"
+	PORT     = 3306
+	DATABASE = "golangtest"
+)
 
 func test(c *gin.Context) {
 	data := new(IndexData)
@@ -57,8 +74,51 @@ func LoginAuth(c *gin.Context) {
 	}
 }
 
+func selectUser(DB *sql.DB, username string) {
+	user := new(User)
+	stmtOut, err := DB.Prepare("SELECT * FROM users WHERE username = ?")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	err = stmtOut.QueryRow(username).Scan(&user.id, &user.username, &user.password)
+	if err != nil {
+		fmt.Println("select user error", err)
+		return
+	}
+	fmt.Println("users", *user)
+}
+
+func insertUser(DB *sql.DB, username, password string) error {
+	_, err := DB.Exec(`INSERT INTO users (username, password) VALUES (?, ?)`, username, password)
+	if err != nil {
+		fmt.Println("err", err)
+		return errors.New("insert users error")
+	}
+	return nil
+}
+
 func main() {
 	server := gin.Default()
+
+	// db connection
+	db, err := sql.Open("mysql", "root:password@/demo")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	if err != nil {
+		fmt.Println("開啟 MySQL 連線發生錯誤，原因為：", err)
+		return
+	}
+
+	// Open doesn't open a connection. Validate DSN data:
+	if err = db.Ping(); err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	insertUser(db, "user2", "password")
+	selectUser(db, "user2")
 
 	// set the path to read HTML file
 	server.LoadHTMLGlob("template/html/*")
