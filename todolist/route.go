7bc2c9825db774a2c1ef3af0409072b0552301af
kemp <kemp@wistron.com>
2022-11-01 09:37:21 +0800

    refactor: 拆分module

---
+++ todolist/route.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	api "go-demo/todolist/api"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initRouter() *gin.Engine {
+	server := gin.Default()
+
+	// router
+	v1 := server.Group("/dev/api/v1")
+	{
+		v1.GET("/todos", api.GetTodoLists)
+		v1.GET("/todos/:id", api.GetTodoList)
+		v1.POST("/todos", api.PostTodo)
+		v1.PUT("/todos/:id", api.PutTodo)
+		v1.DELETE("/todos/:id", api.DeleteTodo)
+	}
+
+	return server
+}
